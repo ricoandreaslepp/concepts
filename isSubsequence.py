f840d6c8aa34f7500d8605e8dfb12298deab13dd
ricoandreaslepp <52963102+ricoandreaslepp@users.noreply.github.com>
2021-09-23 12:51:34 +0300

    Create isSubsequence.py

---
+++ isSubsequence.py
@@ -0,0 +1,14 @@
+def isSubsequence(s, t):
+
+    i, j = 0, 0
+    
+    while i < len(s) and j < len(t):
+        if s[i]==t[j]:
+            j += 1
+        i += 1
+
+    # if j reached the end of string t
+    return j==len(t)
+  
+s, t = input("Whole string: "), input("Sequence to check: ")
+print("%s %s a subsequence of %s" % (t, "is" if isSubsequence(s, t) else "is not", s))
