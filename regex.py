fff6c9aa28fe27ac3a4aed7dc4d744685777390a
ricoandreaslepp <52963102+ricoandreaslepp@users.noreply.github.com>
2021-12-11 15:19:33 +0200

    Update regex.py

--- regex.py
+++ regex.py
@@ -1,5 +1,8 @@
-import re
-
 # regex pattern matching for emails
 def isValidEmail(s):       
+    import re
     return False if not re.match(r'[a-zA-Z0-9_-]+@[a-zA-Z0-9]+.[a-zA-Z]{1,3}$',s) else True
+
+def slice(s, n):
+    from textwrap import wrap
+    return wrap(s, n)
