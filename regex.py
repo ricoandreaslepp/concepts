6d85c63cf89caea75c606a1ea534ab175dc47060
ricoandreaslepp <52963102+ricoandreaslepp@users.noreply.github.com>
2021-12-21 19:00:58 +0200

    added num2words

--- regex.py
+++ regex.py
@@ -6,3 +6,42 @@ def isValidEmail(s):
 def slice(s, n):
     from textwrap import wrap
     return wrap(s, n)
+
+# currently only works <=1000
+# returns a list
+def num2words(n):
+    
+    import re
+
+    digits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+    decine = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+    
+    result = []
+    s = str(n)
+    while True:
+        
+        if n==1000:
+            result = ["one", "thousand"]
+            break
+            
+        elif n >= 100:
+            result.append(digits[int(s[0])])
+            result.append("hundred")
+            
+            if sum(map(int, s[1:])) != 0:
+                result.append("and")
+            
+        elif n >= 20:
+            result.append(decine[int(s[0])-2])
+                
+        elif n > 0:
+            result.append(digits[n])
+            break
+    
+        s = str(n)[1:]
+        n = int(s)
+        
+        if not s:
+            break
+    
+    return result
