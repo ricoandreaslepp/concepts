7f5d431a4554501e49bcc139cc738806c6f80ef9
ricoandreaslepp <52963102+ricoandreaslepp@users.noreply.github.com>
2022-01-08 16:22:19 +0200

    Create for_else.py

---
+++ for_else.py
@@ -0,0 +1,11 @@
+for i in range(10):
+  if i==2:
+    print("1. break")
+    break
+else:
+  print("1. executed the whole loop")
+  
+for i in range(10):
+  pass
+else:
+  print("2. executed the whole loop")
