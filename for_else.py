4b3d112d605d3039f97ad8768c4462c2d4175957
ricoandreaslepp <52963102+ricoandreaslepp@users.noreply.github.com>
2022-01-15 01:13:38 +0200

    Update for_else.py

--- for_else.py
+++ for_else.py
@@ -9,3 +9,17 @@ for i in range(10):
   pass
 else:
   print("2. executed the whole loop")
+  
+  
+# useful in nested loops:
+for i in range(15):
+  
+  for j in range(3):
+    
+    print(f"i, j {i, j}")
+    if i==3 and j==2:
+      break
+      
+  else:
+    continue
+  break
