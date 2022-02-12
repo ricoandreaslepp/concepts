fbcb85a82c7b7a2449a5a3a69f0b1352d1f6d96b
ricoandreaslepp <52963102+ricoandreaslepp@users.noreply.github.com>
2022-02-12 19:52:22 +0200

    Add files via upload

---
+++ tips.py
@@ -0,0 +1,28 @@
+import logging
+import time
+
+start = time.perf_counter()
+
+def testing():
+	# check types with isinstance
+	x = 2
+	if isinstance(x, int):
+		logging.debug(f"x is {x}") # use string formatting
+
+	a = [1,2,3]
+	b = [4,5,6]
+	if a and b: # don't need anything else
+
+		for i, (ai, bi) in enumerate(zip(a, b)):
+			logging.info(f"{i} {(ai, bi)}")
+
+def main():
+	level = logging.DEBUG
+	fmt = '[%(levelname)s] %(asctime)s - %(message)s'
+	logging.basicConfig(level=level, format=fmt)
+
+	testing()
+
+if __name__ == "__main__": # useful if you need to reuse stuff from different files
+	main()
+	print(time.perf_counter() - start) # use perf_counter for timing your code
