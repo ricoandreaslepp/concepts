19965dc5d5b0c624773ebbbb0c286134c755bd69
ricoandreaslepp <52963102+ricoandreaslepp@users.noreply.github.com>
2022-07-06 16:40:50 +0300

    Update and rename regex.py to int_to_words.py

--- int_to_words.py
+++ int_to_words.py
@@ -1,14 +1,3 @@
-# regex pattern matching for emails
-def isValidEmail(s : "email string") -> "True or False email":
-    import re
-    return False if not re.match(r'[a-zA-Z0-9_-]+@[a-zA-Z0-9]+.[a-zA-Z]{1,3}$',s) else True
-
-# from textwrap official docs:
-# Wraps the single paragraph in text so every line is at most width characters long. .
-def slice(s : "input string", n : "width of line") -> "a list of output lines, without final newlines":
-    from textwrap import wrap
-    return wrap(s, width)
-
 # currently only works <=1000
 # int n, returns a list
 def num2words(n : "integer input") -> "list of strings":
