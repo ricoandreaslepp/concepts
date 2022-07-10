ef17b813a5ae50694a8e10c10a3e50f5daa00279
ricoandreaslepp <52963102+ricoandreaslepp@users.noreply.github.com>
2022-07-10 18:29:08 +0300

    Update maths.py

--- maths.py
+++ maths.py
@@ -4,8 +4,18 @@ def gcd(a, b):
     if (b==0): return a
     return gcd(b, a%b)
 
-def extended_gcd():
-    return None
+# pu + qv = gcd(p, q)
+# pu + qv = 1 # take modulo q both sides
+# pu + qv = 1 (mod q) # qv (mod q) is always 0
+# pu ~ 1 (mod q) # which is modular inverse
+
+# Extended gcd
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = egcd(b % a, a)
+        return (g, x - (b // a) * y, y)
 
 # simple lcm
 def lcm(a, b)
